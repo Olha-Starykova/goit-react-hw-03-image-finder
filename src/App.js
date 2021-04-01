d884d447d9f705364d05718615d1a7a64563b710
Olha-Starykova <olenkalog@gmail.com>
2021-04-02 02:25:39 +0300

    com

--- src/App.js
+++ src/App.js
@@ -1,12 +1,64 @@
+import React, { Component } from 'react';
+ import Searchbar from './components/Searchbar/Searchbar'
+ import axios from 'axios';
 
 
-
-function App() {
-  return (
-    <div>
-      Привет
-    </div>
-  );
+class App extends Component {
+  state = {
+    hits: [],
+    currentPage: 1,
+    searchQuery: '',
+  };
+  //получаем данные
+  componentDidMount() {
+    
 }
 
+ 
+  //данные с сабмита
+  onChangeQuery = query => {
+    // console.log(query)
+   this.fetchHits(query)
+   this.setState({searchQuery: query})
+  };
+
+  fetchHits = (query) => {
+     const{currentPage}=this.state
+      axios.get(`https:pixabay.com/api/?q=${query}&per_page=12&&key=20314649-0be4b13706b99da5b0e7a5a44&image_type=photo&orientation=horizontalpage=${currentPage}`)
+      .then(response => {
+        console.log(response.data.hits);
+        this.setState(prevState=> ({
+          hits: response.data.hits,
+          currentPage: prevState.currentPage + 1,
+        }));
+      });
+  }
+
+// webformatURL - ссылка на маленькое изображение для списка карточек
+// largeImageURL - ссылка на большое изображение для модального окна
+
+  render() {
+    const {hits} = this.state
+    return (
+      <div>
+   
+        <Searchbar onSubmit={this.onChangeQuery} />
+        <ul>
+          {hits.map(({ id, webformatURL }) =>
+            <li key={id}>
+            
+              <img src={webformatURL} alt={webformatURL} />
+              
+            </li>)}
+        </ul>
+  <button type='button' onClick={this.fetchHits}>Загрузить еще</button>
+
+      </div>
+    );
+    }
+};
+
 export default App;
+
+
+
---
+++ src/components/Button/Button.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// import { button } from './Button.module.css';
+
+const Button = ({  }) => {
+
+  return (
+    <button type="button" className={button} onClick={handelClick}>
+      Load more
+    </button>
+  );
+};
+
+Button.propTypes = {
+  serviceSearchImages: PropTypes.func.isRequired,
+};
+
+export default Button;
